6e9214836836a5db3349c94248402401cbf64ba6
WilliamPhilippe <wplb@ic.ufal.br>
2018-12-26 01:22:41 -0300

    Update list.cpp

--- Estruturas/Linked List/list.cpp
+++ Estruturas/Linked List/list.cpp
@@ -16,9 +16,36 @@ node *CreatList(){
 
 }
 
+void AddItem(node *list, int x){
+
+	node *new_pointer = (node *) malloc(sizeof(node));
+	new_pointer->i = x;
+	new_pointer->next = NULL;
+
+	node *pointer = list;
+
+	while(pointer->next != NULL){
+		pointer = pointer->next;
+	}
+
+	pointer->next = new_pointer;
+}
+
+void PrintList(node *list){
+	node *pointer = list->next;
+	while(1){
+		cout << pointer->i << endl;
+		if(pointer->next == NULL) return;
+		else pointer = pointer->next;
+	}
+}
+
 int main(){
 
 	node *list = CreatList();
+	AddItem(list, 10);
+	AddItem(list, 25);
+	PrintList(list);
 
 
 	return 0;
