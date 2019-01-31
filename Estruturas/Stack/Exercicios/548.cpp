ecbc4b3a3cd61272b45bc96e0b06a40d48702083
William Philippe <wplb@ic.ufal.br>
2019-01-31 08:49:40 -0300

    Update 548.cpp

--- Estruturas/Stack/Exercicios/548.cpp
+++ Estruturas/Stack/Exercicios/548.cpp
@@ -4,12 +4,13 @@ using namespace std;
 struct nodeList
 {
 	int value;
-	nodeList *next;
+	nodeList *next, *previous;
 };
 
 struct nodeStack
 {
 	nodeList *headList;
+	nodeList *backList;
 	nodeStack *next;
 };
 
@@ -20,14 +21,51 @@ struct setStack
 
 void pushStack(setStack *stack, nodeStack *nodeNewStack){
 
+	if(stack->headStack == NULL){
+		stack->headStack = nodeNewStack;
+		nodeNewStack->next = NULL;
+	}
+	else{
+		nodeNewStack->next = stack->headStack;
+		stack->headStack = nodeNewStack;
+	}
+
 }
 
-void pushList(nodeList *lista, int x){
+void pushList(nodeStack *pilha, int x){
+
+	nodeList *newNode = (nodeList *) malloc(sizeof(nodeList));
+	newNode->value = x;
+
+	if(pilha->headList == NULL){
+		pilha->headList = newNode;
+		pilha->backList = newNode;
+		newNode->next = NULL;
+		newNode->previous = NULL;
+	}	
+	else{
+		newNode->next = NULL;
+		newNode->previous = pilha->backList;
+		pilha->backList->next = newNode;
+		pilha->backList = newNode;
+	}
 
 }
 
 void popStack(setStack *stack){
 
+}
+
+void printStackList(nodeStack *node){
+	nodeList *current = node->headList;
+
+	cout << current->value;
+	current = current->next;
+	while(current != NULL){
+		cout << " " << current->value;
+		current = current->next;
+	}
+	cout << endl;
 }
 
 
@@ -40,25 +78,29 @@ int main(){
 	stack->headStack = NULL;
 
 	while(gets(command)){
-		cout << command << endl;
 
 		if(strcmp(command, "PUSH") == 0){
 			nodeStack *nodeNewStack = (nodeStack *) malloc(sizeof(nodeStack));
 			nodeNewStack->headList = NULL;
+			nodeNewStack->backList = NULL;
 
 			while(1){
 				scanf("%d%c", &x, &trash);
-				pushList(nodeNewStack->headList, x);
+				pushList(nodeNewStack, x);
 
 				if(trash == '\n') break;
 			}
 
-			pushStack(stack, nodeNewStack); 
+			pushStack(stack, nodeNewStack);
 		}
 		else if(strcmp(command, "POP") == 0){
-
+			
+			if(stack->headStack == NULL) cout << "EMPTY STACK" << endl;
+			else{
+				printStackList(stack->headStack);
+				stack->headStack = stack->headStack->next;
+			}
 		}
-		else{ cout << "error";  }
 	}
 
 
