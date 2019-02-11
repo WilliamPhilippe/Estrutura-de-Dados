080896fcc11a59f3471ad5d786fde582a729276e
William Philippe <wplb@ic.ufal.br>
2019-02-11 15:18:28 -0300

    Update 414.cpp

--- Estruturas/Priority Queue/Exercícios/414.cpp
+++ Estruturas/Priority Queue/Exercícios/414.cpp
@@ -2,23 +2,57 @@
 using namespace std;
 
 struct node { int nota; node *next, *previous; };
-struct setPQ { node *head; };
+struct setPQ { node *head, *back; int max, min; };
 
 void InsertItem(setPQ *PQ, int p){
 	node *newNode = (node *) malloc(sizeof(node));
 	newNode->nota = p;
 
 	if(PQ->head == NULL){
-		PQ->head = newNode;
 		newNode->next = NULL;
+		newNode->previous = NULL;
+		PQ->head = newNode;
+		PQ->back = newNode;
+		PQ->max = PQ->min = p;
+	}
+	else if(newNode->nota >= PQ->max){
+		newNode->next = PQ->head;
+		newNode->previous = NULL;
+		newNode->next->previous = newNode;
+		PQ->head = newNode;
+		PQ->max = p;
+	}
+	else if(newNode->nota <= PQ->min){
+		newNode->next = NULL;
+		newNode->previous = PQ->back;
+		newNode->previous->next = newNode;
+		PQ->back = newNode;
+		PQ->min = p;
 	}
 	else{
 		node *current = PQ->head;
+		while(current->nota > p) current = current->next;
 
-		while(current->next != NULL && current->nota > newNode->nota){
-
-		}
+		newNode->next = current;
+		newNode->previous = current->previous;
+		current->previous = newNode;
+		newNode->previous->next = newNode;
 	}
+}
+
+int verificar(int *alunos, int m, setPQ *PQ){
+	int contador = 0;
+	node *current = PQ->head;
+	for(int i = 0; i < m; i++, current = current->next)
+		if(current->nota == alunos[i]) contador++;
+	return contador;
+}
+
+void printqueue(setPQ *PQ){
+	node *current = PQ->head;
+
+	while(current != NULL) cout << current->nota << " ", current = current->next;
+	cout << endl;
 }
 
 int main(){
@@ -35,6 +69,8 @@ int main(){
 		PQ->head = NULL;
 
 		for(int i = 0; i < m; i++) InsertItem(PQ, alunos[i]);
+		int cc = verificar(alunos, m, PQ);
+		cout << cc << endl;
 	}
 
 	return 0;
