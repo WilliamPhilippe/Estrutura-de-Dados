09c7bf1c0606e82f44d1d626e5075058cd4b67d8
WilliamPhilippe <wplb@ic.ufal.br>
2019-02-16 17:23:22 -0300

    up

--- Estruturas/Árvore/547.cpp
+++ Estruturas/Árvore/547.cpp
@@ -1,51 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct nodeT{ int value; nodeT *right, *left; };
-struct nodeS{ nodeT *elemment; nodeS *next; };
-struct SetStack{ nodeS *head; };
+struct nodeTree{ int value; nodeTree *right, *left; };
 
-SetStack *CreateStack(){
-	SetStack *stack = (SetStack *) malloc(sizeof(SetStack));
-	stack->head = NULL;
-	return stack;
+int returnNumber(char *s, int *pos){
+	int i, number = 0;
+	for(i = *pos; s[i] >= 48 && s[i] <= 57; i++) number = (number*10) + (s[i] - 48);
+	*pos = i;
+	return number;
 }
 
-nodeT *PopStack (SetStack *stack){
-	if(stack->head == NULL) return NULL;
+void returnTree(char *s, int *pos, nodeTree **no){
+
+	if(s[*pos] == '(' && s[*pos + 1] == ')'){ (*pos) += 2; *no = NULL; return; }
+	else if(s[*pos] == ')'){ (*pos) ++; return; }
 	else{
-		nodeS *current = stack->head;
-		stack->head = current->next;
-		return current->elemment;
+		(*pos) ++;
+		int number = returnNumber(s, pos);	
+		nodeTree *newNode = (nodeTree *) malloc(sizeof(nodeTree));
+		newNode->value = number;
+		*no = newNode;
+
+		returnTree(s, pos, &((*no)->left) );
+		returnTree(s, pos, &((*no)->right) );
+		(*pos) ++;
 	}
+
 }
 
-void PushStack (SetStack *stack, nodeT *node){
-	nodeS *newNode = (nodeS *) malloc(sizeof(nodeS));
-	newNode->elemment = node;
+nodeTree *returnTree(char *s){
+	nodeTree *root = (nodeTree *) malloc(sizeof(nodeTree));
+	int pos = 1;
+	root->value = returnNumber(s, &pos);
 
-	newNode->next = stack->head;
-	stack->head = newNode;
+	returnTree(s, &pos, &(root->left) );
+	returnTree(s, &pos, &(root->right) );
+
+	return root;
 }
 
-nodeT *buildTree(char *s){
-
-	SetStack *stack = CreateStack();
-	nodeT *root = NULL, *current = NULL;
-
-	bool value;
-	for(int i = 0; s[i] != '\0'; i++){
-		if(s[i] == '(') current = PopStack(stack);
-		else if(s[i] >= 48 && s[i] <= 57){
-			
-		}
-	} 
+void dfs(nodeTree *node, int v, int *p, int pTemp){
+	if(node == NULL) return;
+	else if(node->value == v) { *p = pTemp; return; }
+	else{
+		dfs(node->left, v, p, pTemp + 1);
+		dfs(node->right, v, p, pTemp + 1);
+	}
 }
 
 int main(){
 	
-	char s[10000]; gets(s);
-	nodeT *root = buildTree(s);
+	char s[100000]; gets(s);
+	int no; cin >> no;
+
+	nodeTree *root = returnTree(s);
+
+	int p = -1;
+	dfs(root, no, &p, 0);
+
+	if(p == -1) cout << "NAO ESTA NA ARVORE\n" << -1 << endl;
+	else cout << "ESTA NA ARVORE\n" << p << endl;
 
 	return 0;
 }
